83570f36e614bb0f37e1beb92c3a4a44125cc6eb
sszp <aaaaaa1964027610@163.com>
2024-03-08 02:45:35 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import typing
+
+
+if __name__ == '__main__':
+    pass
---
+++ test.py
@@ -0,0 +1,10 @@
+
+def print_hello():
+    print('Hello World')
+
+
+if __name__ == '__main__':
+    import main
+    main.print_info([1, 2, 3, 4])
+
+    
